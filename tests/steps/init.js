d3b16f7d43ead5edd529519cf1fbae19ef17b990
theburningmonk <theburningmonk@gmail.com>
2018-01-06 14:17:40 +0000

    - fixed acceptance test

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -2,6 +2,7 @@
 
 const co = require('co');
 const Promise = require('bluebird');
+const awscred = Promise.promisifyAll(require('awscred'));
 
 let initialized = false;
 
@@ -16,6 +17,13 @@ let init = co.wrap(function* () {
   process.env.cognito_client_id = "test_cognito_client_id";
   process.env.cognito_user_pool_id = "us-east-1_DfuAwa0vB";
   process.env.cognito_server_client_id = "niv7esuaibla0tj5q36b6mvnr";
+
+  if (!process.env.AWS_ACCESS_KEY_ID) {
+    let cred = (yield awscred.loadAsync()).credentials;
+    
+    process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+  }
 
   initialized = true;
 });
