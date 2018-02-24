3cba8487e7a5482105b028b52bfafa799a882aee
theburningmonk <theburningmonk@gmail.com>
2018-02-24 00:15:45 +0000

    - added retry logic for notify-restaurant

--- functions/notify-restaurant.js
+++ functions/notify-restaurant.js
@@ -1,38 +1,20 @@
 'use strict';
 
-const _ = require('lodash');
 const co = require('co');
-const AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-const sns = new AWS.SNS();
 const getRecords = require('../lib/kinesis').getRecords;
-const streamName = process.env.order_events_stream;
-const topicArn = process.env.restaurant_notification_topic;
+const notify = require('../lib/notify');
+const retry = require('../lib/retry');
 
 module.exports.handler = co.wrap(function* (event, context, cb) {
   let records = getRecords(event);
   let orderPlaced = records.filter(r => r.eventType === 'order_placed');
 
   for (let order of orderPlaced) {
-    let pubReq = {
-      Message: JSON.stringify(order),
-      TopicArn: topicArn
-    };
-    yield sns.publish(pubReq).promise();
-
-    console.log(`notified restaurant [${order.restaurantName}] of order [${order.orderId}]`);
-
-    let data = _.clone(order);
-    data.eventType = 'restaurant_notified';
-    
-    let putRecordReq = {
-      Data: JSON.stringify(data),
-      PartitionKey: order.orderId,
-      StreamName: streamName
-    };
-    yield kinesis.putRecord(putRecordReq).promise();
-
-    console.log(`published 'restaurant_notified' event to Kinesis`);
+    try {
+      yield notify.restaurantOfOrder(order);
+    } catch (err) {
+      yield retry.restaurantNotification(order);
+    }
   }
 
   cb(null, 'all done');
