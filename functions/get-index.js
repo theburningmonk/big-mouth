192964549c5450fd44dcbf8ecab169f633b363f1
Yan Cui <theburningmonk@gmail.com>
2018-06-03 11:08:25 -0700

    - pass correlation IDs through HTTP

--- functions/get-index.js
+++ functions/get-index.js
@@ -4,7 +4,7 @@ const co             = require("co");
 const Promise        = require("bluebird");
 const fs             = Promise.promisifyAll(require("fs"));
 const Mustache       = require('mustache');
-const http           = require('superagent-promise')(require('superagent'), Promise);
+const http           = require('../lib/http');
 const URL            = require('url');
 const aws4           = require('../lib/aws4');
 const log            = require('../lib/log');
@@ -41,15 +41,10 @@ function* getRestaurants() {
 
   aws4.sign(opts);
 
-  let httpReq = http
-    .get(restaurantsApiRoot)
-    .set('Host', opts.headers['Host'])
-    .set('X-Amz-Date', opts.headers['X-Amz-Date'])
-    .set('Authorization', opts.headers['Authorization']);
-
-  if (opts.headers['X-Amz-Security-Token']) {
-    httpReq.set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token']);
-  } 
+  let httpReq = http({
+    uri: restaurantsApiRoot,
+    headers: opts.headers
+  });
 
   return new Promise((resolve, reject) => {
     let f = co.wrap(function*(subsegment) {
@@ -110,5 +105,5 @@ const handler = co.wrap(function* (event, context, callback) {
 });
 
 module.exports.handler = middy(handler)
-  .use(correlationIds({ sampleDebugLogRate: 0.01 }))
+  .use(correlationIds({ sampleDebugLogRate: 0.9 }))
   .use(sampleLogging({ sampleRate: 0.01 }));
